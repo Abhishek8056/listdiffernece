d4989ac5133a486e149586701b0b3d51a071b583
abhhishek singh <singhabhi1746@gmail.com>
2023-07-06 20:04:56 +0530

    list difference

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const ListDifferenceApp = () => {
+  const [listA, setListA] = useState('');
+  const [listB, setListB] = useState('');
+  const [result, setResult] = useState({
+    onlyInA: [],
+    onlyInB: [],
+    inBoth: [],
+    combined: []
+  });
+
+  const computeDifferences = () => {
+    const arrA = listA.split('\n').map(item => item.trim());
+    const arrB = listB.split('\n').map(item => item.trim());
+
+    const onlyInA = arrA.filter(item => !arrB.includes(item));
+    const onlyInB = arrB.filter(item => !arrA.includes(item));
+    const inBoth = arrA.filter(item => arrB.includes(item));
+    const combined = [...new Set([...arrA, ...arrB])];
+
+    setResult({
+      onlyInA,
+      onlyInB,
+      inBoth,
+      combined
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <div data-reactroot>
+        <div>
+          <div class="container">
+            <div>
+              <h2>List Difference Application</h2>
+              <div>
+                <label>List A:</label>
+                <textarea
+                  rows={5}
+                  value={listA}
+                  onChange={e => setListA(e.target.value)}
+                />
+              </div>
+              <div>
+                <label>List B:</label>
+                <textarea
+                  rows={5}
+                  value={listB}
+                  onChange={e => setListB(e.target.value)}
+                />
+              </div>
+              <button onClick={computeDifferences}>Compute</button>
+              <h3>Results:</h3>
+              <div>
+                <h4>Items present only in A:</h4>
+                <ul>
+                  {result.onlyInA.map(item => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h4>Items present only in B:</h4>
+                <ul>
+                  {result.onlyInB.map(item => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h4>Items present in both A and B:</h4>
+                <ul>
+                  {result.inBoth.map(item => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h4>Items combining both A and B (unique):</h4>
+                <ul>
+                  {result.combined.map(item => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
 
-export default App;
+  );
+};
+
+export default ListDifferenceApp;
